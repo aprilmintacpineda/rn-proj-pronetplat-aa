425eaeb4d9871fcda8062d691d02f1706ad0764d
April Mintac Pineda <aprilmintacpineda@gmail.com>
2020-12-13 22:15:03 +0800

    logout;

--- src/navigations/MainDrawerNavigation.js
+++ src/navigations/MainDrawerNavigation.js
@@ -1,9 +1,11 @@
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import React from 'react';
 import { Drawer as RNPDrawer } from 'react-native-paper';
+import AntDesign from 'react-native-vector-icons/AntDesign';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import useWindowDimensions from 'react-native/Libraries/Utilities/useWindowDimensions';
 import MainStackNavigation from './MainStackNavigation';
+import { logout } from 'fluxible/actions/user';
 
 const Drawer = createDrawerNavigator();
 
@@ -12,7 +14,7 @@ function drawerContent (props) {
   const { routes, index } = state;
   const { navigate } = navigation;
 
-  return routes.map(({ key, name }, i) => {
+  const drawerItems = routes.map(({ key, name }, i) => {
     const { options } = descriptors[key];
     const { title, drawerIcon } = options;
 
@@ -28,6 +30,17 @@ function drawerContent (props) {
       />
     );
   });
+
+  return (
+    <>
+      {drawerItems}
+      <RNPDrawer.Item
+        onPress={logout}
+        label="Logout"
+        icon={props => <AntDesign name="logout" {...props} />}
+      />
+    </>
+  );
 }
 
 function MainDrawerNavigation () {
