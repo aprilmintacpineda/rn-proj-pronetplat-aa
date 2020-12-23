80068a55a648532d4c33f6b2614e3999225c85e5
April Mintac Pineda <aprilmintacpineda@gmail.com>
2020-12-23 20:34:41 +0800

    register screen

--- src/Screens/guest/Register/index.js
+++ src/Screens/guest/Register/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import { Alert } from 'react-native';
 import RegisterForm from './RegisterForm';
-// import { navigationRef } from 'App';
+import { navigationRef } from 'App';
 import FormWithContext from 'components/FormWithContext';
 import { showSuccessPopup } from 'fluxible/actions/popup';
 import { unknownError } from 'libs/alerts';
@@ -26,16 +25,13 @@ const formOptions = {
   },
   ignoreResponse: true,
   endPoint: '/register',
-  onSubmitError: ({ error }) => {
-    if (error.status === 409) Alert.alert(null, 'Email has already been registered.');
-    else unknownError();
-  },
+  onSubmitError: unknownError,
   onSubmitSuccess: () => {
     showSuccessPopup({
       message:
         'You\'re account has bee created. Please check your inbox for the confirmation code that you need to verify your email'
     });
-    // navigationRef.current.navigate('Login');
+    navigationRef.current.navigate('Login');
   }
 };
 
