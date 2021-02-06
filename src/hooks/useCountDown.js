b52255613515abf4273d4c3a4c7c686513152c09
April Mintac Pineda <aprilmintacpineda@gmail.com>
2021-02-06 16:30:50 +0800

    improvements;

--- src/hooks/useCountDown.js
+++ src/hooks/useCountDown.js
@@ -3,59 +3,58 @@ import intervalToDuration from 'date-fns/intervalToDuration';
 import isPast from 'date-fns/isPast';
 import React from 'react';
 
-function useCountDown ({
-  start = null,
-  duration = null,
-  toTime = null
-}) {
-  const timer = React.useRef(null);
+function calcTime ({ start = null, duration = null, toTime = null }) {
+  let isDone = true;
+  let countDownEnd = null;
+  let hours = '00';
+  let minutes = '00';
+  let seconds = '00';
 
-  const [state, setState] = React.useState({
-    isDone: false,
-    timeLeftStr: ''
-  });
+  if (start || duration || toTime) {
+    countDownEnd = toTime
+      ? new Date(toTime)
+      : add(new Date(start), duration);
+    isDone = isPast(countDownEnd);
+  }
+
+  if (!isDone) {
+    const interval = intervalToDuration({
+      start: new Date(),
+      end: countDownEnd
+    });
+
+    hours = interval.hours.toString().padStart(2, '0');
+    minutes = interval.minutes.toString().padStart(2, '0');
+    seconds = interval.seconds.toString().padStart(2, '0');
+  }
+
+  return {
+    isDone,
+    timeLeftStr: isDone ? '' : `${hours}:${minutes}:${seconds}`
+  };
+}
+
+function useCountDown (props) {
+  const timer = React.useRef(null);
+  const [state, setState] = React.useState(() => calcTime(props));
 
   const count = React.useCallback(() => {
-    let isDone = true;
-    let countDownEnd = null;
-    let hours = '00';
-    let minutes = '00';
-    let seconds = '00';
+    const timerState = calcTime(props);
+    if (!timerState.isDone) timer.current = setTimeout(count, 500);
+    setState(timerState);
+  }, [props]);
 
-    if (start || duration || toTime) {
-      countDownEnd = toTime
-        ? new Date(toTime)
-        : add(new Date(start), duration);
-      isDone = isPast(countDownEnd);
-    }
-
-    if (!isDone) {
-      const interval = intervalToDuration({
-        start: new Date(),
-        end: countDownEnd
-      });
-
-      hours = interval.hours.toString().padStart(2, '0');
-      minutes = interval.minutes.toString().padStart(2, '0');
-      seconds = interval.seconds.toString().padStart(2, 0);
-    } else {
-      clearInterval(timer.current);
-    }
-
-    setState({
-      isDone,
-      timeLeftStr: isDone ? '' : `${hours}:${minutes}:${seconds}`
-    });
-  }, [start, duration, toTime]);
+  const { isDone } = state;
 
   React.useEffect(() => {
-    count();
-    timer.current = setInterval(count, 500);
+    if (isDone) return;
+
+    timer.current = setTimeout(count, 500);
 
     return () => {
-      clearInterval(timer.current);
+      clearTimeout(timer.current);
     };
-  }, [count]);
+  }, [isDone, count]);
 
   return state;
 }
