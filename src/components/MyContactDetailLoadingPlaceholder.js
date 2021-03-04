f3ab6f3391384a936d04862f54ead57732f8e1fb
April Mintac Pineda <aprilmintacpineda@gmail.com>
2021-03-04 15:42:35 +0800

    fixed placeholders

--- src/components/MyContactDetailLoadingPlaceholder.js
+++ src/components/MyContactDetailLoadingPlaceholder.js
@@ -1,11 +1,6 @@
 import React from 'react';
 import { View } from 'react-native';
-import {
-  Fade,
-  Placeholder,
-  PlaceholderLine,
-  PlaceholderMedia
-} from 'rn-placeholder';
+import { Fade, Placeholder, PlaceholderLine } from 'rn-placeholder';
 import ListItemSeparator from './ListItemSeparator';
 
 function ContactDetailLoadingPlaceholder ({
@@ -18,23 +13,23 @@ function ContactDetailLoadingPlaceholder ({
         {!isFirstFetch && <ListItemSeparator />}
         <Placeholder Animation={Fade}>
           <View style={{ margin: 15 }}>
-            <PlaceholderLine width={30} height={6} />
             <View
               style={{
-                marginLeft: 15,
                 flexDirection: 'row',
                 justifyContent: 'space-between',
                 alignItems: 'center'
               }}
             >
               <View style={{ flex: 1 }}>
-                <PlaceholderLine width={80} />
+                <PlaceholderLine
+                  width={80}
+                  height={20}
+                  style={{ marginBottom: 10 }}
+                />
+                <PlaceholderLine width={60} height={10} />
                 <PlaceholderLine width={40} height={6} />
               </View>
-              <PlaceholderMedia
-                style={{ borderRadius: 100 }}
-                size={50}
-              />
+              <PlaceholderLine width={10} height={10} />
             </View>
           </View>
         </Placeholder>
--- src/env.example.js
+++ src/env.example.js
@@ -1,3 +1,3 @@
-export const STAGE = 'local';
+export const STAGE = 'prod';
 export const API_BASE_URL =
   'https://luh5c5ormk.execute-api.ap-southeast-1.amazonaws.com/dev/';
