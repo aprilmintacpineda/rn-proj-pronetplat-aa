a475338856dde2fb72739fa52cb498589d49a04b
April Mintac Pineda <aprilmintacpineda@gmail.com>
2021-03-10 17:46:24 +0800

    removed reactotron

--- ReactotronConfig.js
+++
@@ -1,9 +0,0 @@
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import Reactotron from 'reactotron-react-native';
-
-Reactotron.setAsyncStorageHandler(AsyncStorage)
-  .configure({
-    host: '192.168.1.5'
-  })
-  .useReactNative()
-  .connect();
--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-if (__DEV__) require('./ReactotronConfig');
-
 import 'react-native-gesture-handler';
 import { AppRegistry } from 'react-native';
 import { name as appName } from './app.json';
