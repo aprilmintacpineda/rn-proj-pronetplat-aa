fe5d7afc39cf17d72b7810fbd156034c788e6e57
April Mintac Pineda <aprilmintacpineda@gmail.com>
2021-03-15 19:18:01 +0800

    app banner

--- src/Screens/guest/Login/Authenticate/AuthenticateForm.js
+++ src/Screens/guest/Login/Authenticate/AuthenticateForm.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { ScrollView, View } from 'react-native';
-import { Headline, Subheading } from 'react-native-paper';
 import { FormContext } from 'components/FormWithContext';
 import Button from 'components/FormWithContext/Button';
 import SubmitButton from 'components/FormWithContext/SubmitButton';
 import TextInput from 'components/FormWithContext/TextInput';
 import LoginContext from 'root/Screens/guest/Login/LoginContext';
+import HeadName from 'svgs/HeadName';
 import Logo from 'svgs/Logo';
+import SubHeadName from 'svgs/SubHeadName';
 import { paperTheme } from 'theme';
 
 function AuthenticateForm ({ onLogin }) {
@@ -39,12 +40,8 @@ function AuthenticateForm ({ onLogin }) {
           }}
         >
           <Logo width={100} height={100} />
-          <Headline style={{ marginTop: 30, color: '#fff' }}>
-            Entrepic
-          </Headline>
-          <Subheading style={{ color: '#fff' }}>
-            Make professional connection
-          </Subheading>
+          <HeadName style={{ marginVertical: 15 }} />
+          <SubHeadName />
         </View>
         <View style={{ margin: 20 }}>
           <TextInput field="email" type="email" />
