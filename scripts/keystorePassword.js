7a1b523efd891557728a41295a559da58ed36633
April Mintac Pineda <aprilmintacpineda@gmail.com>
2021-03-17 20:24:08 +0800

    updates

---
+++ scripts/keystorePassword.js
@@ -0,0 +1,15 @@
+const fs = require('fs').promises;
+const path = require('path');
+
+(async () => {
+  const filePath = path.join(
+    __dirname,
+    '../android/gradle.properties'
+  );
+  let fileContents = await fs.readFile(filePath, 'utf-8');
+  fileContents = fileContents.replaceAll(
+    '__KEYSTORE_PASSWORD__',
+    process.env.KEYSTORE_PASSWORD
+  );
+  await fs.writeFile(filePath, fileContents);
+})();
