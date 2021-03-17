c68bbe2d32e718dec51d684f7074e67f67fcdc3c
April Mintac Pineda <aprilmintacpineda@gmail.com>
2021-03-17 19:19:40 +0800

    deployment

--- babel.config.js
+++ babel.config.js
@@ -16,22 +16,7 @@ const plugins = [
 if (process.env.NODE_ENV === 'production') {
   plugins.push(
     'react-native-paper/babel', // react-native-paper
-    'transform-remove-console',
-    [
-      'search-and-replace',
-      {
-        rules: [
-          {
-            search: /__STAGE__/gm,
-            replace: process.env.STAGE
-          },
-          {
-            search: /__API_BASE_URL__/gm,
-            replace: process.env.API_BASE_URL
-          }
-        ]
-      }
-    ]
+    'transform-remove-console'
   );
 }
 
---
+++ scripts/replacer.js
@@ -0,0 +1,20 @@
+const fs = require('fs').promises;
+const path = require('path');
+const cmdArgs = require('command-line-args');
+
+(async () => {
+  const optionDefinitions = [
+    { name: 'file', alias: 'f', type: String },
+    { name: 'placeholder', alias: 'p', type: String },
+    { name: 'replacement', alias: 'r', type: String }
+  ];
+
+  const { file, placeholder, replacement } = cmdArgs(
+    optionDefinitions
+  );
+
+  const filePath = path.join(__dirname, '../', file);
+  let fileContent = await fs.readFile(filePath, 'utf-8');
+  fileContent = fileContent.replaceAll(placeholder, replacement);
+  await fs.writeFile(filePath, fileContent);
+})();
