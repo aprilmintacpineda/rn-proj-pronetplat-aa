463dd70cbd60e4437f94cfac469d36f94bb1aaa4
April Mintac Pineda <aprilmintacpineda@gmail.com>
2021-04-12 20:48:48 +0800

    Privacy policy and terms and conditions links;

--- src/Screens/guest/Login/Authenticate/AuthenticateForm.js
+++ src/Screens/guest/Login/Authenticate/AuthenticateForm.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { ScrollView, View } from 'react-native';
+import { Text } from 'react-native-paper';
 import { FormContext } from 'components/FormWithContext';
 import Button from 'components/FormWithContext/Button';
 import SubmitButton from 'components/FormWithContext/SubmitButton';
 import TextInput from 'components/FormWithContext/TextInput';
+import TextLink from 'components/TextLink';
 import LoginContext from 'root/Screens/guest/Login/LoginContext';
 import HeadName from 'svgs/HeadName';
 import Logo from 'svgs/Logo';
@@ -46,6 +48,19 @@ function AuthenticateForm ({ onLogin }) {
         <View style={{ margin: 20 }}>
           <TextInput field="email" type="email" />
           <TextInput field="password" type="password" />
+          <View style={{ marginBottom: 20 }}>
+            <Text>
+              Please take some time to read our{' '}
+              <TextLink to="https://entrepic.com/privacy-policy">
+                privacy policy
+              </TextLink>{' '}
+              and{' '}
+              <TextLink to="https://entrepic.com/terms-and-conditions">
+                terms and conditions
+              </TextLink>
+              .
+            </Text>
+          </View>
           <SubmitButton>Submit</SubmitButton>
           <View style={{ marginTop: 50, marginBottom: 20 }}>
             <Button style={{ marginBottom: 10 }} to="Register">
