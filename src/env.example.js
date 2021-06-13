582d1b2269064e89b6bda83a3f06366dc49ec743
April Mintac Pineda <aprilmintacpineda@gmail.com>
2021-06-13 19:11:59 +0800

    updated websocket endpoint

--- src/env.example.js
+++ src/env.example.js
@@ -1,3 +1,4 @@
 export const STAGE = 'dev';
 export const API_BASE_URL = 'https://apiv1dev.entrepic.com';
-export const WEBSOCKET_URL = 'wss://websocketdev.entrepic.com';
+export const WEBSOCKET_URL =
+  'wss://i1rrsnnag7.execute-api.ap-southeast-1.amazonaws.com/dev';
