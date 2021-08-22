e121c4c4f18dad93d029b9ea25b6c86410c85f9a
April Mintac Pineda <aprilmintacpineda@gmail.com>
2021-08-22 11:29:22 +0800

    no need for fromData and toData

--- src/Screens/auth/Events/View/Discussion/EditHistoryRow.js
+++ src/Screens/auth/Events/View/Discussion/EditHistoryRow.js
@@ -4,17 +4,10 @@ import { Text } from 'react-native-paper';
 import Caption from 'components/Caption';
 import TimeAgo from 'components/TimeAgo';
 
-function EditHistoryRow ({ fromData, toData, createdAt }) {
+function EditHistoryRow ({ data, createdAt }) {
   return (
     <View style={{ paddingVertical: 10 }}>
-      <Text>
-        <Text style={{ fontWeight: 'bold' }}>From: </Text>
-        <Text>{fromData.comment}</Text>
-      </Text>
-      <Text style={{ marginTop: 5 }}>
-        <Text style={{ fontWeight: 'bold' }}>To: </Text>
-        <Text>{toData.comment}</Text>
-      </Text>
+      <Text>{data.comment}</Text>
       <Caption style={{ marginTop: 5 }}>
         <TimeAgo dateFrom={createdAt} />
       </Caption>
